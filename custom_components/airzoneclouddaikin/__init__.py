142bc4ff7bbc0377f6ac7e5970348fa58d4032c9
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-03-15 09:12:01 +0100

    Update __init__.py

--- custom_components/airzoneclouddaikin/__init__.py
+++ custom_components/airzoneclouddaikin/__init__.py
@@ -1,1 +1,21 @@
-"""Airzone Cloud Daikin integration"""
+"""DKN Cloud for HASS integration."""
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "airzoneclouddaikin"
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up DKN Cloud for HASS from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    _LOGGER.info("DKN Cloud for HASS integration successfully configured.")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if entry.entry_id in hass.data[DOMAIN]:
+        del hass.data[DOMAIN][entry.entry_id]
+    return True
