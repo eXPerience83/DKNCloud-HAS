ff862c5b49a56073a90e27be37adb245ea814ca6
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-03-19 09:53:12 +0100

    Update __init__.py
    async_forward_entry_setup changes. v0.2.3

--- custom_components/airzoneclouddaikin/__init__.py
+++ custom_components/airzoneclouddaikin/__init__.py
@@ -1,30 +1,11 @@
-"""DKN Cloud for HASS integration."""
-import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "airzoneclouddaikin"
+from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up DKN Cloud for HASS from a config entry."""
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-    # Forward the setup to the climate and sensor platforms.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    _LOGGER.info("DKN Cloud for HASS integration configured successfully.")
-    return True
+    hass.data[DOMAIN][entry.entry_id] = AirzoneCloudDaikinAPI(entry.data)
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "climate")
-    unload_ok = unload_ok and await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate", "sensor"])
