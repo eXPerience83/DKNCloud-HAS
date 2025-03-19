a119b86f1fa744528df785ea65c15011226f54d6
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-03-19 17:23:32 +0100

    Update const.py

--- custom_components/airzoneclouddaikin/const.py
+++ custom_components/airzoneclouddaikin/const.py
@@ -7,7 +7,7 @@ DOMAIN = "airzoneclouddaikin"
 CONF_USERNAME = "username"      # Replace with your Airzone Cloud account email when configuring
 CONF_PASSWORD = "password"      # Replace with your Airzone Cloud account password when configuring
 
-# API Endpoints (as defined in the original package)
+# API Endpoints as defined in the original package
 API_LOGIN = "/users/sign_in"
 API_INSTALLATION_RELATIONS = "/installation_relations"
 API_DEVICES = "/devices"
